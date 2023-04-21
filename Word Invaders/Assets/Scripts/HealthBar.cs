abb1c506b94fd9e17e4fa4690ebfb34f259ab3b8
Martin Edgar Atole <meatole.cs@gmail.com>
2023-04-22 02:48:31 +0800

    fix: life system error

--- Word Invaders/Assets/Scripts/HealthBar.cs
+++ Word Invaders/Assets/Scripts/HealthBar.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HealthBarScript : MonoBehaviour
+public class HealthBar : MonoBehaviour
 {
      /// <Note> Will be used for HealthBar calculation
     public Slider slider;
